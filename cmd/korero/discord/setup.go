b27a336ecf78c6a21c2c3134b32d1d39bc55a3af
cdoern <cdoern@redhat.com>
2022-01-20 11:46:07 -0500

    korero setup work
    Signed-off-by: cdoern <cdoern@redhat.com>

--- cmd/korero/discord/setup.go
+++ cmd/korero/discord/setup.go
@@ -43,13 +43,13 @@ func setup(cmd *cobra.Command, args []string) error {
 			"are you setting up a discord API client? (y/n): ")
 		response, _ := reader.ReadString('\n')
 		evalResponse(response, "y\n")
-		fmt.Print("1) go to https://discord.com/developers/applications and click \"New Application\" give your bot a name! type c to to continue.")
+		fmt.Print("\n1) go to https://discord.com/developers/applications and click \"New Application\" give your bot a name! type c to to continue. ")
 		response, _ = reader.ReadString('\n')
 		evalResponse(response, "c\n")
-		fmt.Print("Navigate to the side bar and click the \"Bot\" tab, then click \"Add Bot\" and confirm. press c to continue.")
+		fmt.Print("\n2) Navigate to the side bar and click the \"Bot\" tab, then click \"Add Bot\" and confirm. type c to continue. ")
 		response, _ = reader.ReadString('\n')
 		evalResponse(response, "c\n")
-		fmt.Print("Make sure your bot is public, copy the bot's token and enter it here: ")
+		fmt.Print("\n3) Make sure your bot is public, copy the bot's token and enter it here: ")
 		Token, _ = reader.ReadString('\n')
 		Token = strings.Replace(Token, "\n", "", -1)
 		os.Setenv("KORERO_DISCORD_TOKEN", Token)
@@ -58,25 +58,24 @@ func setup(cmd *cobra.Command, args []string) error {
 		if err != nil {
 			return err
 		}
-		fmt.Println("Login Successful: ", dg.Identify.Token)
-		fmt.Print("Great! now to invite your bot, navitage to the OAuth2 General tab on the webpage. Under \"Default Authorization Link\" select \"In-app Authorization\". type c to continue.")
+		fmt.Println("\nLogin Successful: ", dg.Identify.Token)
+		fmt.Print("\n4) Great! now to invite your bot, navitage to the OAuth2 General tab on the webpage. Under \"Default Authorization Link\" select \"In-app Authorization\". type c to continue. ")
 		response, _ = reader.ReadString('\n')
 		evalResponse(response, "c\n")
-		fmt.Print("Under Scopes click \"bot\" and \"applications.commands\". In the bot permissions table, select all of the commands you wish your bot to have! type c to continue")
+		fmt.Print("\n5) Under Scopes click \"bot\" and \"applications.commands\". In the bot permissions table, select all of the commands you wish your bot to have! type c to continue. ")
 		response, _ = reader.ReadString('\n')
 		evalResponse(response, "c\n")
-		fmt.Print("Navigate to the OAuth2 URL Generator. Again select \"bot\" and \"applications.commands\" then choose your permissions again to generate the URL. This url can then be used to add your bot to any server you own or can be distributed to any server who you know the owner of. type c to finish setup.")
+		fmt.Print("\n6) Navigate to the OAuth2 URL Generator. Again select \"bot\" and \"applications.commands\" then choose your permissions again to generate the URL. This url can then be used to add your bot to any server you own or can be distributed to any server who you know the owner of. type c to finish setup. ")
 		response, _ = reader.ReadString('\n')
 		evalResponse(response, "c\n")
 	} else {
-		fmt.Println("Using provided token....")
+		fmt.Println("\nUsing provided token....")
 		dg, err := utils.LoginDiscord(Token)
 		if err != nil {
 			return err
 		}
-		fmt.Println("Login Successful: ", dg.Identify.Token)
+		fmt.Println("\nLogin Successful: ", dg.Identify.Token)
 	}
-
 	return nil
 }
 
